64c4deaec5c5b2a44aa1e10740bf2bcd4b471af2
SimoRBLX <91323541+SimoRBLX@users.noreply.github.com>
2022-12-23 08:26:36 +0100

    *

--- src/main/java/com/simosforgemods/forgemod/ForgeMod.java
+++ src/main/java/com/simosforgemods/forgemod/ForgeMod.java
@@ -76,8 +76,7 @@ public class ForgeMod {
     public static class ClientModEvents {
 
         @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event)
-        {
+        public static void onClientSetup(FMLClientSetupEvent event) {
             // Some client setup code
             LOGGER.info("HELLO FROM CLIENT SETUP");
             LOGGER.info("MINECRAFT NAME >> {}", Minecraft.getInstance().getUser().getName());
